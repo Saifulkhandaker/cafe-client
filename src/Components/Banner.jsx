7308a04c7ea1c3669fdb7ed74271cd430cc168a4
Saifulkhandaker <khsaiful67@gmail.com>
2024-01-01 18:01:58 +0600

    banner update

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,37 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import banner1 from '../assets/Images/Banner/Banner1.jpg'
+import banner2 from '../assets/Images/Banner/Banner2.jpg'
+import { Link } from 'react-router-dom';
+
+const Banner = () => {
+    return (
+        <div className='relative'>
+                {/* img section */}
+                <Swiper 
+                    pagination={{
+                    dynamicBullets: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper md:h-screen"
+                    autoplay={{ delay: 1000}}
+                >
+                    <SwiperSlide><img src={banner1} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={banner2} alt="" /></SwiperSlide>
+                    
+                </Swiper>
+            
+                {/* text section */}
+            <div className='w-[80%] mx-auto text-white text-center space-y-3 absolute inset-0 z-10 flex flex-col justify-center items-center '>
+                <h1 className='text-2xl lg:text-7xl '>Coffee is all about <br />
+                <span className='font-bold'>People, Care & Passion</span></h1>
+                <p className='text-sm lg:text-lg'>From this Assembly Line, Hitek has been producing more than 1000 machines & <br />equipments and continuously manufactures high quality product to meet our various customer needs.</p>
+                <Link to="/aboutUs"><button className='btn bg-[#B78D62] text-white border-none hover:bg-[#593D25] lg:text-xl rounded-l-full rounded-r-full'>READ MORE</button></Link>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import logo from "../../assets/Images/Others/logo.png"
+import logo from "../../assets/Images/Others/logo-white.png"
 import { HiShoppingCart } from "react-icons/hi";
 
 
@@ -12,14 +12,14 @@ const Navbar = () => {
             <li><NavLink className="text-[16px] font-medium rounded-sm " to="/contactUs">Contact Us</NavLink></li>
         </>
     return (
-        <div className="w-[95%] mx-auto border-b-2" >
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
+        <div className="text-white border-b" >
+            <div className="navbar bg-[#3D291C] lg:px-10">
+                <div className="navbar-start z-30 ">
+                    <div className="dropdown ">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="bg-[#3D291C] menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {navlinks}
                     </ul>
                     </div>
@@ -38,7 +38,7 @@ const Navbar = () => {
                             <div className="badge badge-secondary">+0</div>
                         </button>
                     </Link>
-                    <Link to="/login"><button className="text-[16px] font-medium rounded-sm border btn">Login</button></Link>
+                    <Link to="/login"><button className="text-[16px] text-white rounded-lg bg-[#B78D62] border-none  font-medium rounded-sm border btn">Login</button></Link>
                 </div>
             </div>
         </div>
