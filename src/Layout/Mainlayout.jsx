0fa75b3e7c0ad834b87ba4c6b53b6a499ca55c9d
Saifulkhandaker <khsaiful67@gmail.com>
2024-01-01 13:13:50 +0600

    primary setup complete

---
+++ src/Layout/Mainlayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mainlayout = () => {
+    return (
+        <div>
+            This is home
+        </div>
+    );
+};
+
+export default Mainlayout;
---
+++ src/Route/Route.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import Mainlayout from "../Layout/Mainlayout";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Mainlayout></Mainlayout>
+    }
+])
