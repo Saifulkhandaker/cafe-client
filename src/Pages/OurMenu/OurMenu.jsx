de924d55d838b573a6524991025204bffce34b48
Saifulkhandaker <khsaiful67@gmail.com>
2024-01-08 21:59:33 +0600

    menu serction added

--- src/Pages/OurMenu/OurMenu.jsx
+++ src/Pages/OurMenu/OurMenu.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet';
 import Cover from '../Shared/Cover';
-import menucover from '../../assets/Images/Others/Menu Cover.jpg'
+import menucover from '../../assets/Images/Others/Menu Cover.jpg';
+import menu from '../../assets/Images/Others/Menu.png'
 
 const OurMenu = () => {
     return (
@@ -9,6 +10,12 @@ const OurMenu = () => {
                 <title>NyaBiss | Menu</title>
             </Helmet>
             <Cover img={menucover} title={"Our Menu"}></Cover>
+            {/* Menu section */}
+            <div className='w-[90%] mx-auto space-y-10 py-28'>
+                <h1 className='shared text-center text-3xl md:text-6xl '>Drinks & Beverages</h1>
+                <p className='text-center md:text-2xl text-[#363636] font-sans'>Drinks and beverages encompass a vast array of liquid consumables, <br /> from refreshing juices and sodas to energizing coffees and soothing teas. <br />They quench thirst, provide diverse flavors, and often serve <br /> as social catalysts, enhancing various culinary experiences.</p>
+                <img className='w-full lg:pt-10' src={menu} alt="" />
+            </div>
         </div>
     );
 };
