a70250ea09bd0ff1693b5576489335cc1dcb7aeb
Saifulkhandaker <khsaiful67@gmail.com>
2024-01-08 19:39:07 +0600

    contact form added in contact us page

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -4,8 +4,30 @@ import coverimg from '../../assets/Images/Others/cover.jpg';
 import { FaLocationDot, FaPhone } from "react-icons/fa6";
 import { HiOutlineMailOpen } from "react-icons/hi";
 import { MdAccessTimeFilled } from "react-icons/md";
+import { useRef } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ContactUs = () => {
+
+    const formRef = useRef();
+    
+    const handlesend = (e) => {
+        e.preventDefault();
+        toast.success('Email Send Successfully...!', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+            formRef.current.reset();
+        
+    }
+
     return (
         <div>
             <Helmet>
@@ -44,6 +66,51 @@ const ContactUs = () => {
                     <h3 className='text-xl font-medium text-[#949090]'>Tuesday - <br />Sunday <br />10:00am - <br />11:30pm</h3>
                 </div>
             </div>
+            {/* contact form */}
+            <div className='flex flex-col justify-center text-center items-center'>
+                <h3 className='text-xl lg:text-2xl font-bold text-[#C8A077] mt-16'>IF YOU HAVE ANY QUESTION</h3>
+                <h1 className='text-4xl lg:text-6xl font-medium text-[#111111]'>Leave us a messages</h1>
+                <form onSubmit={handlesend} ref={formRef} className='w-[70%] lg:w-[60%] mx-auto my-20 border shadow-2xl py-10 px-5 md:px-20'>
+                    <label className="form-control w-full">
+                        <div className="label">
+                            <span className="label-text text-2xl font-medium text-[#3E3E3E]">Name *</span>
+                        </div>
+                        <input type="text" required className="input border-[#E6E6E6] border-2 w-full md:h-20 rounded-none" />
+                    </label>
+                    <label className="form-control w-full">
+                        <div className="label">
+                            <span className="label-text text-2xl font-medium text-[#3E3E3E]">Email *</span>
+                        </div>
+                        <input type="email" required className="input border-[#E6E6E6] border-2 w-full md:h-20 rounded-none" />
+                    </label>
+                    <label className="form-control w-full">
+                        <div className="label">
+                            <span className="label-text text-2xl font-medium text-[#3E3E3E]">Subject *</span>
+                        </div>
+                        <input type="text" required className="input border-[#E6E6E6] border-2 w-full md:h-20 rounded-none" />
+                    </label>
+                    <label className="form-control w-full">
+                        <div className="label">
+                            <span className="label-text text-2xl font-medium text-[#3E3E3E]">Message *</span>
+                        </div>
+                        <textarea required className="input  border-[#E6E6E6] border-2 w-full h-20 md:h-36 rounded-none" />
+                    </label>
+                    <input className='bg-[#1a1919] hover:bg-[#3E3E3E] text-white text-lg font-bold py-6 w-full mt-3 rounded-full' type="submit" value="SEND EMAIL" />
+                </form>
+            </div>
+            <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            theme="light"
+            />
+            {/* Same as */}
+            <ToastContainer />
         </div>
     );
 };
