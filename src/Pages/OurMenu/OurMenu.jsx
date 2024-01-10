ab1c4c4e742d7e65eed6d43ef1106db3c75f5be2
Saifulkhandaker <khsaiful67@gmail.com>
2024-01-10 21:33:36 +0600

    animation added in menu page

--- src/Pages/OurMenu/OurMenu.jsx
+++ src/Pages/OurMenu/OurMenu.jsx
@@ -12,9 +12,9 @@ const OurMenu = () => {
             <Cover img={menucover} title={"Our Menu"}></Cover>
             {/* Menu section */}
             <div className='w-[90%] mx-auto space-y-10 py-28'>
-                <h1 className='shared text-center text-3xl md:text-6xl '>Drinks & Beverages</h1>
-                <p className='text-center md:text-2xl text-[#363636] font-sans'>Drinks and beverages encompass a vast array of liquid consumables, <br /> from refreshing juices and sodas to energizing coffees and soothing teas. <br />They quench thirst, provide diverse flavors, and often serve <br /> as social catalysts, enhancing various culinary experiences.</p>
-                <img className='w-full lg:pt-10' src={menu} alt="" />
+                <h1 className='shared text-center text-3xl md:text-6xl' data-aos="fade-down" data-aos-duration="2000">Drinks & Beverages</h1>
+                <p className='text-center md:text-2xl text-[#363636] font-sans' data-aos="fade-down" data-aos-duration="2000">Drinks and beverages encompass a vast array of liquid consumables, <br /> from refreshing juices and sodas to energizing coffees and soothing teas. <br />They quench thirst, provide diverse flavors, and often serve <br /> as social catalysts, enhancing various culinary experiences.</p>
+                <img className='w-full lg:pt-10' data-aos="zoom-in" data-aos-duration="2000" src={menu} alt="" />
             </div>
         </div>
     );
